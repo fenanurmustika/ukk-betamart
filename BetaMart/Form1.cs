ede9f5959d9099d4cf64cc6d6e6dca32f1f22970
fenamustika88@gmail.com <fenamustika88@gmail.com>
2021-04-26 14:07:04 +0700

    first commit

---
+++ BetaMart/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BetaMart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (string.IsNullOrEmpty(txtSearch.Text))
+                {
+                    this.barangTableAdapter.Fill(this.appData.barang);
+                    barangBindingSource.DataSource = this.appData.barang;
+                    // dataGridView.DataSource = barangBindingSource;
+                }
+                else
+                {
+                    var query = from o in this.appData.barang
+                                where o.NamaBarang.Contains(txtSearch.Text) || o.KodeBarang.Contains(txtSearch.Text) || o.Satuan.Contains(txtSearch.Text)
+                                select o;
+                    barangBindingSource.DataSource = query.ToList();
+                    // dataGridView.DataSource = query.ToList();
+                }
+            }
+        }
+
+        private void dataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+#pragma warning disable CS0642 // Possible mistaken empty statement
+                if (MessageBox.Show("Are you sure want to delete this record ?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) ;
+#pragma warning restore CS0642 // Possible mistaken empty statement
+                barangBindingSource.RemoveCurrent();
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                panel.Enabled = true;
+                this.appData.barang.AddbarangRow(this.appData.barang.NewbarangRow());
+                barangBindingSource.MoveLast();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                barangBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = true;
+            txtKodeBarang.Focus();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            panel.Enabled = false;
+            barangBindingSource.ResetBindings(false);
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                barangBindingSource.EndEdit();
+                barangTableAdapter.Update(this.appData.barang);
+                panel.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                barangBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.barangTableAdapter.Fill(this.appData.barang);
+            barangBindingSource.DataSource = this.appData.barang;
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Apakah anda akan menghapus data nya?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                barangBindingSource.RemoveCurrent();
+        }
+    }
+}
---
+++ BetaMart/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:BetaMart.Form1.txtSearch_KeyPress(System.Object,System.Windows.Forms.KeyPressEventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:BetaMart.Form1.dataGridView_KeyDown(System.Object,System.Windows.Forms.KeyEventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:BetaMart.Form1.btnNew_Click(System.Object,System.EventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:BetaMart.Form1.btnEdit_Click(System.Object,System.EventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:BetaMart.Form1.btnCancel_Click(System.Object,System.EventArgs)")]
